a02104af433b0df6d553aa64d54644e583d12a9b
TheAlexBig <00062816@uca.edu.sv>
2018-07-21 08:35:18 -0600

    Working to make a callback and timer

---
+++ SocialMedia/VideoPost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia
+{
+    class VideoPost: Post {
+        private string VideoURL { get; set; }
+        private int LonguitudVideo { get; set; }
+
+        public VideoPost(string titulo, bool espublico, string usuarioemisor, string url, int longuitud )
+        {
+            this.Titulo = titulo;
+            this.EsPublico = espublico;
+            this.UsuarioEmisor = usuarioemisor;
+            this.VideoURL = url;
+            this.LonguitudVideo = longuitud;
+        }
+    }
+}
